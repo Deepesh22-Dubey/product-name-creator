3cf26619c140accd66cab8caadf30909b4c98ff7
Deepesh22-Dubey <deepeshdubey450@gmail.com>
2026-07-16 20:23:47 +0530

    Add Product Name Creator project
    A simple Python program that generates product names from user input.

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to Product Name Creator!")
+brand_name = input("Enter your brand name:")
+model_name = input("Enter the model name:")
+product_name =  brand_name +" " + model_name 
+print("Your Product Name is:")
+print(product_name) 
+print("Thank you for using Product Name Creator!")
